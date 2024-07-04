797b6ffdb365b63f3ab5a982674bcfe4449542a8
Rifki Rahadian <rrahadian94@gmail.com>
2024-07-04 16:50:49 +0700

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
