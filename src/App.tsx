125a62b736b6118215c74c9e33363f85011cf1b3
Rifki Rahadian <rrahadian94@gmail.com>
2024-07-05 09:29:19 +0700

    chore: Base layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import PrivateRoute from './middlewares/auth'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={
+        <PrivateRoute>
+          <Layout>
+            <Home />
+          </Layout>
+        </PrivateRoute>
+      } />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
---
+++ src/components/Layout.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { getUser, logout } from '../services/localStorage';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<any>({});
+
+  useEffect(() => {
+    const data = getUser();
+    setUser(data.user);
+  }, []);
+  
+  
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" fixed="top">
+        <Container>
+          <Navbar.Brand href="/">My App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+            <Navbar.Text className="me-3">
+              Signed in as: <a href="#profile">{user.name}</a>
+            </Navbar.Text>
+            <Button variant="outline-danger" onClick={handleLogout}>
+              Logout
+            </Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="mt-3">
+        {children}
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
